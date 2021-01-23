b3ee77298134bc62aeab1293929214ae82a2bc55
mihailoffplay <72168155+mihailoffplay@users.noreply.github.com>
2021-01-23 19:18:03 +0300

    Добавление аккордеона

---
+++ ex.7_accordion/js/accordion.js
@@ -0,0 +1,19 @@
+var accordion = document.getElementsByClassName("faq_accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
---
+++ ex.7_accordion/js/burger.js
@@ -0,0 +1,3 @@
+document.querySelector('.burger').addEventListener('click', function(){
+    document.querySelector('.burger span').classList.toggle('active');
+  })
