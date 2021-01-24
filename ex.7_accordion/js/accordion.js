fee5f931e9f26c0b0c4c9890f56a2c428d4e0be7
mihailoffplay <72168155+mihailoffplay@users.noreply.github.com>
2021-01-24 22:45:35 +0300

    Update accordion.js

--- ex.7_accordion/js/accordion.js
+++ ex.7_accordion/js/accordion.js
@@ -1,10 +1,10 @@
-var accordion = document.getElementsByClassName("faq_accordion");
-var i;
+let accordion = document.getElementsByClassName("faq_accordion");
+let i;
 
 for (i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
+        let panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
