121378cf5ae7b829ba9c3742b9f51b1f7e9e6e5c
mihailoffplay <72168155+mihailoffplay@users.noreply.github.com>
2021-02-12 22:34:50 +0300

    Валидация+бургер меню

--- ex.8_mobile+validation/js/validation.js
+++ ex.8_mobile+validation/js/validation.js
@@ -1,2 +1,18 @@
-$('#sign-up').validate();
-/* $('#sign-up form').validate(); */
+$('#sign-up form').validate({
+    rules: {
+        name: "required",
+        activity: "required",
+        email: {
+            required: true,
+            email: true
+        }
+    },
+        messages: {
+            name: "Пожалуйста введите своё имя",
+            activity: "Пожалуйста выберите профессию",
+            email: {
+              required: "Нам нужен ваш адрес электронной почты, чтобы связаться с вами",
+              email: "Ваш адрес электронной почты должен быть в формате name@domain.com"
+            }
+          }
+    });
